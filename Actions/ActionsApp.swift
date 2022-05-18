a13191f20986ee3421871a4827c8a2f422412e6f
Damodar <damodar.namala@outlook.com>
2022-05-18 12:04:04 +0530

    Initial Commit

---
+++ Actions/ActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionsApp.swift
+//  Actions
+//
+//  Created by Damodar Namala on 18/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
