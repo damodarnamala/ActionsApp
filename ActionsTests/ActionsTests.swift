e26543fc3c0743b6fb734001c1dd326d974d8731
Damodar <damodar.namala@outlook.com>
2022-05-18 12:07:33 +0530

    added test-case

--- ActionsTests/ActionsTests.swift
+++ ActionsTests/ActionsTests.swift
@@ -18,12 +18,8 @@ class ActionsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testSampleBoolian() throws {
+        XCTAssertTrue( 3 > 1, "3 is always greater than 1")
     }
 
     func testPerformanceExample() throws {
